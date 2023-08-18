ecbea3f90807b4329b0fbb623da667294a250dad
FabioDiCeglie <fabiod@backbase.com>
2023-08-18 11:06:41 +0200

    fix: some imports

--- microservices/inventory-service/src/main/java/com/programming/inventoryservice/controller/InventoryController.java
+++ microservices/inventory-service/src/main/java/com/programming/inventoryservice/controller/InventoryController.java
@@ -3,11 +3,7 @@ package com.programming.inventoryservice.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.programming.inventoryservice.service.InventoryService;
 import com.programming.inventoryservice.dto.InventoryResponse;
